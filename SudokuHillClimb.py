88cd106658b4c76fc900122112e6821b4a876745
mwhumphrey <mwhumphrey@smcm.edu>
2021-02-28 19:21:56 -0800

    More Stable version of Sudoku Hill Climbing
    TODO:
    
    -Get it to loop infinitely until global min is 0 or until we hit limit
    
    -More extensive testing

--- SudokuHillClimb.py
+++ SudokuHillClimb.py
@@ -4,10 +4,12 @@ from SudokuBoard import *
 x = 0 #current x coord
 y = 0 #current y coord
 board = []
+globalMin = 100
 
 
 def climbHills(inputBoard): #string sb to indicate which board is being played i.e. ("sudoku1.csv")
     global board
+    global globalMin
     sb = SudokuBoard(inputBoard)
     board = sb.board #2d array
     ls = sb.lockedSquares #locked squares
@@ -16,14 +18,16 @@ def climbHills(inputBoard): #string sb to indicate which board is being played i
         for y in range(len(board)): 
             if ls[x][y] == True: #if the square is locked, 
                 pass #do nothing
+            if x == 10:
+                return 0 #FOR TESTING
             else: #otherwise we are allowed to edit this square
 
                 print ("current y: " , x, "current x: ", y) #needs to be swapped because board is processed col, row and I didn't realize til now
                 print ("Current Value: ", board[x][y])
                 print (sb)
 
-                column = SudokuBoard.getColumn(sb, x)
-                row = SudokuBoard.getRow(sb,y)
+                column = SudokuBoard.getColumn(sb, y)
+                row = SudokuBoard.getRow(sb,x)
                 region = SudokuBoard.getRegion(sb, (int(y/5)), (int(x/5))) #needs to be swapped because this is processed col, row, in SudokuBoard (at least i think, either way it works)
 
                 # print("\n")
@@ -36,28 +40,28 @@ def climbHills(inputBoard): #string sb to indicate which board is being played i
                 indexCount = 0
                 conflictList = []
                 for j in range(1,26):
-                    conflictList.append(SudokuBoard.countConflicts(sb, j, column, row, region))
+                    if j == currentValue:#if we are at the current val, it is counted 3 too many times
+                        conflictList.append(SudokuBoard.countConflicts(sb, j, column, row, region))
+                        conflictList[-1] = conflictList[-1] - 3
 
-                for j in conflictList:
+                    else:
+                        conflictList.append(SudokuBoard.countConflicts(sb, j, column, row, region))
+
+
+
+                for j in conflictList: #simply for printing visual / understanding what is going on
                     indexCount = indexCount + 1
                     print("Trying: ", indexCount,  "    conflicts:", j)
-
                 
+                minConflicts = min(conflictList)
+                bestVal = conflictList.index(min(conflictList)) + 1
+                print ("min # of conflicts:", minConflicts)
+                print ("best value:", bestVal, "\n")
+                print("\n")
+                board = SudokuBoard.replaceSquare(sb, x, y, bestVal)
 
+                #need some way of continuing until all mins are 0
 
-
-
-
-
-
-
-
-    #count # of total conflicts (instances of the current value) in row, col, and region, this is local min
-    #loop through all numbers 1-25, replacing the current square w/ the number
-
-
-    #board = SudokuBoard.replaceSquare(sb, 0, 0, 99)
-    #print(SudokuBoard.replaceSquare(sb, 0, 0, 99))  how to replace a square
 
 
 
