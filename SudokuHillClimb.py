5c10a93f227b6f796ef04e706c69cc2157f5fab4
mwhumphrey <mwhumphrey@smcm.edu>
2021-03-01 17:53:50 -0800

    Hill climbing - 95% Complete
    -In order to print best possible result I have to make a list of the mins, print smallest min as well as the board associated with it.

--- SudokuHillClimb.py
+++ SudokuHillClimb.py
@@ -1,77 +1,80 @@
 from SudokuBoard import *
 
-
-x = 0 #current x coord
-y = 0 #current y coord
 board = []
-globalMin = 100
+globalMin = 100 #
+conflictCollection = [100] #a list of all of the conflict values, if the sum = 0 then we have completed the game
+maxIterations = 50 #the number of times we iterate over the entire board
+Iterations = 0 #current number of iterations
 
 
 def climbHills(inputBoard): #string sb to indicate which board is being played i.e. ("sudoku1.csv")
     global board
-    global globalMin
+    global conflictCollection
+    global Iterations
+    global maxIterations
     sb = SudokuBoard(inputBoard)
     board = sb.board #2d array
     ls = sb.lockedSquares #locked squares
     
-    for x in range(len(board)): 
-        for y in range(len(board)): 
-            if ls[x][y] == True: #if the square is locked, 
-                pass #do nothing
-            else: #otherwise we are allowed to edit this square
+    while sum(conflictCollection) != 0 and Iterations < maxIterations * 100: #Just finished an iteration, and the globalMin is not 0
+        conflictCollection.clear()  #clear conflicts for that iteration, iterate again
 
-                print ("current y: " , x, "current x: ", y) #needs to be swapped because board is processed col, row and I didn't realize til now
-                print ("Current Value: ", board[x][y])
-                print (sb)
+        for x in range(len(board)):      #begins looping through the board, switching values for more favorable ones
+            for y in range(len(board)): 
+                if ls[x][y] == True:     #if the square is locked, 
+                    pass                 #do nothing
+                else:                    #otherwise we are allowed to edit this square
 
-                column = SudokuBoard.getColumn(sb, y)
-                row = SudokuBoard.getRow(sb,x)
-                region = SudokuBoard.getRegion(sb, (int(y/5)), (int(x/5))) #needs to be swapped because this is processed col, row, in SudokuBoard (at least i think, either way it works)
+                    print ("current y: " , x, "current x: ", y) #needs to be swapped because board is processed col, row and I didn't realize til now
+                    print ("Current Value: ", board[x][y])
+                    print (sb)
 
-                # print("\n")
-                print ("Current Column: ", column)
-                print ("Current Row: ", row)
-                print ("Current Region: ", region) #currently broken
-                # print ("y/5:", int(x/5), "    x/5:", int(y/5))
+                    column = SudokuBoard.getColumn(sb, y)
+                    row = SudokuBoard.getRow(sb,x)
+                    region = SudokuBoard.getRegion(sb, (int(y/5)), (int(x/5))) #needs to be swapped because this is processed col, row, in SudokuBoard
 
-                currentValue = board[x][y]
-                indexCount = 0
-                conflictList = []
-                for j in range(1,26):
-                    if j == currentValue:#if we are at the current val, it is counted 3 too many times
-                        conflictList.append(SudokuBoard.countConflicts(sb, j, column, row, region))
-                        conflictList[-1] = conflictList[-1] - 3
+                    print ("Current Column: ", column)
+                    print ("Current Row: ", row)
+                    print ("Current Region: ", region) 
 
-                    else:
-                        conflictList.append(SudokuBoard.countConflicts(sb, j, column, row, region))
+                    currentValue = board[x][y]
+                    indexCount = 0
+                    conflictList = []
 
-
-
-                for j in conflictList: #simply for printing visual / understanding what is going on
-                    indexCount = indexCount + 1
-                    print("Trying: ", indexCount,  "    conflicts:", j)
-                
-                minConflicts = min(conflictList)
-                bestVal = conflictList.index(min(conflictList)) + 1
-                print ("min # of conflicts:", minConflicts)
-                print ("best value:", bestVal, "\n")
-                print("\n")
-                board = SudokuBoard.replaceSquare(sb, x, y, bestVal)
-
-                #need some way of continuing until all mins are 0
+                    for j in range(1,26):
+                        if j == currentValue:#if we are at the current val, it is counted 3 too many times
+                            conflictList.append(SudokuBoard.countConflicts(sb, j, column, row, region))
+                            conflictList[-1] = conflictList[-1] - 3
+                        else:
+                            conflictList.append(SudokuBoard.countConflicts(sb, j, column, row, region))
 
 
+                    for j in conflictList: #simply used for printing visual / understanding what is going on
+                        indexCount = indexCount + 1
+                        print("Trying: ", indexCount,  "    conflicts:", j)
+                    
+                    minConflicts = min(conflictList)
+                    bestVal = conflictList.index(min(conflictList)) + 1
+                    print ("min # of conflicts:", minConflicts) #minconflicts is local min
+                    conflictCollection.append(minConflicts) #in order to calculate global min
+                    print ("best value:", bestVal, "\n")
+                    print("\n")
+                    board = SudokuBoard.replaceSquare(sb, x, y, bestVal)
+
+                    Iterations = Iterations + 1 #to keep track of the # of iterations
+
+
+    if sum(conflictCollection) == 0:
+        print("Game complete in ", Iterations , " iterations")
+        print("Final Board:")
+        print (sb)
+
+    if Iterations >= maxIterations * 100:
+        print("Game ended due to max iteration limit of ", Iterations)
+        print("Global min upon failure: ", sum(conflictCollection))
+        print("Final Board:")
+        print (sb)
 
 
 
-
-
-
-
-climbHills("sudoku1.csv")
-
-#we are at starting position, are we locked? if not:
-    #replace current value, with every value 1-25
-    #with each of these, look at # of conflicts using getRow, getColumn, and getRegion, update heuristic (global_min, local_min)
-    #pick swap that had the least conflicts. This is best choice
-    #do this until all conflicts = 0 for every [x][y] or until we get stuck at a global min
+climbHills("sudoku2.csv")
